5624decd7339dc0c13e3c1fe68323f2c524c451e
MaximKonovodov <maxim.konovodov@gmail.com>
2020-04-07 15:22:41 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const app = require('./app')
+
+const server = (port) => app.listen(port, (err) => {
+    if (err) {
+        return console.log('server falls')
+    } else {
+        return console.log(`server is running on ${port}`)
+    };
+});
+
+server(4000);
