474a9d3d730468f64fba2dd0a8f30ead9d6bf6e6
MaximKonovodov <maxim.konovodov@gmail.com>
2020-04-08 00:19:05 +0300

    node response with api

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-const express = require('express')
+const books = require('./books.json')
 
+const express = require('express')
+const cors = require('cors')
 const app = express();
+
+app.use(cors())
+
+app.use(express.static('public'));
+
+app.get("/", function (request, response) {
+  response.send('go to /books to get books')
+});
+
+app.get("/books", function (request, response) {
+  return response.json({
+    books
+  })
+});
+
+app.get(`/book/id:bookId`, function (request, response) {
+  let book = books.find(book => `${book.id}` === `${request.params.bookId}`)
+  return response.json({
+    book
+  })
+});
 
 module.exports = app;
