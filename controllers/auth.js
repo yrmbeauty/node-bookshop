4834fffd482cfd92cd36fc60dea2cef5f6f238e0
MaximKonovodov <maxim.konovodov@gmail.com>
2020-08-12 13:06:37 +0300

    auth & books controllers

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,24 +1,135 @@
-const register = (req, res) => {
-  try {
-    const { email, pass } = req.body
-    
-  } catch (err) {
-    return res.status(500).json({ message: err.message });
-  }
-}
+const BCRYPT = require("bcrypt");
+const { validationResult } = require("express-validator");
+const JWT = require("jsonwebtoken");
+const CONFIG = require("../config/default.json");
 
-const login = (req, res) => {
-  try {
-    // if (!book) {
-    //   return res.status(404).json({ message: `Book with ${bookId} not found` })
-    // }
-    // return res.json({ book })
-  } catch (err) {
-    return res.status(500).json({ message: err.message });
-  }
-}
+const db = require("../models");
 
-module.exports = { 
-  register,
+const registration = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400)
+        .json(
+          {
+            message: "Некорректные данные при регистрации",
+            errors: errors.array(),
+          }
+        );
+    }
+
+    const { email, password } = req.body;
+
+    const candidate = await db.user
+      .findOne(
+        { where: { email } }
+      );
+
+    if (candidate) {
+      return res.status(400)
+        .json(
+          { message: "Такой пользователь уже существует" }
+        );
+    }
+
+    const hashedPassword = await BCRYPT
+      .hash(password, 12);
+
+    const user = await db.user
+      .create(
+        { email, password: hashedPassword }
+      );
+
+    return res.status(201)
+      .json(
+        { message: `Пользователь ${email} создан` }
+      );
+
+  } catch (err) {
+    return res
+      .status(500)
+      .json(
+        {
+          message: err.message
+        }
+      );
+  }
+};
+// LOGIN-------------------------------------------------->
+const login = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty) {
+      return res.status(400)
+        .json(
+          {
+            errors: errors.array(),
+            message: "Некорректные данные при входе в систему"
+          }
+        );
+    }
+
+    const { email, password } = req.body;
+
+    const user = await db.user
+      .findOne(
+        {
+          where: { email }
+        }
+      );
+
+    if (!user) {
+      return res
+        .status(400)
+        .json(
+          {
+            message: "Пользователь не найден"
+          }
+        );
+    }
+
+    const isMatch = await BCRYPT
+      .compare(password, user.password);
+
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json(
+          {
+            message: "Неверный пароль, попробуйет снова"
+          }
+        );
+    }
+
+    const token = JWT
+      .sign(
+        { userId: user.id },
+        CONFIG.get("jwtSecret"),
+        { expiresIn: "1h" }
+      );
+
+    return res
+      .status(200)
+      .json(
+        {
+          token, userId: user.id
+        }
+      );
+
+  } catch (err) {
+    return res
+      .status(500)
+      .json(
+        {
+          message: err.message
+        }
+      );
+  }
+};
+
+module.exports = {
+  registration,
   login
-}
+};
--- controllers/book.js
+++ controllers/book.js
@@ -1,47 +1,47 @@
-const db = require('../models');
+const db = require("../models");
 
-const books = require('../books.json')
+const books = require("../books.json");
 
 const getBook = (req, res) => {
   try {
     const { bookId } = req.params;
-    book = books.find((book) => book.id == bookId)
-    // const book = await db.book.findByPk(bookId);
+    const book = books.find((book) => book.id == bookId);
+    // const book = await db.book.findOne(bookId);
     if (!book) {
-      return res.status(404).json({ message: `Book with ${bookId} not found` })
+      return res.status(404).json({ message: `Book with ${bookId} not found` });
     }
-    return res.json({ book })
+    return res.json({ book });
   } catch (err) {
     return res.status(500).json({ message: err.message });
   }
-}
+};
 
 const getBooks = (req, res) => {
   try {
     const { all, category, author, price } = req.params;
-    // const books = await db.book.findByPk(bookId);
+    // const books = await db.book.findOne(bookId);
     if (!books) {
-      return res.status(404).json({ message: `Books not found` })
+      return res.status(404).json({ message: "Books not found" });
     }
-    return res.json({ books })
+    return res.json({ books });
   } catch (err) {
     return res.status(500).json({ message: err.message });
   }
-}
+};
 
 const createBook = async (req, res) => {
   try {
     const { book } = req.params;
     const resBook = await db.book.create(book);
     
-    return res.status(200).json( resBook instanceof db.book)
+    return res.status(200).json( resBook instanceof db.book);
   } catch (err) {
     return res.status(500).json({ message: err.message });
   }
-}
+};
 
 module.exports = {
   getBook,
   getBooks,
   createBook
-}
+};
