e201de37203a9495cd62941f7793d19b2f7d1af6
MaximKonovodov <maxim.konovodov@gmail.com>
2020-08-12 13:05:49 +0300

    user model

---
+++ models/user.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, Sequelize) => {
+  const User = sequelize.define(
+    "user",
+    {
+      name: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        unique: true
+      },
+      login: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        unique: true
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      downloaded_books: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        unique: true,
+      },
+      books_rating: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        unique: true
+      },
+    }, 
+  );
+  return User;
+};
