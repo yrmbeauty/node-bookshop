1cf89ff281fa2f21f20a35e06a8dd726de5871f6
MaximKonovodov <ravenheart.sp@gmail.com>
2020-09-11 16:26:56 +0300

    filter

--- controllers/book.filter.js
+++ controllers/book.filter.js
@@ -1,59 +1,54 @@
-const db = require("../models");
+const db = require('../models');
 const { Op } = db.Sequelize;
 const DEFAULT_LIMIT = 10;
-// const DEFAULT_PAGE = 1;
+const DEFAULT_PAGE = 1;
 
 const bookFilter = (params) => {
-  const { limit, page, author, category, price } = params;
-  const whereObj = {};
-  const confObj = {};
-  const include = [];
+  const { limit, page, author, category, price, userId } = params;
+  const filter = {
+    where: {},
+    include: [],
+  };
 
   if (parseInt(limit) !== 0) {
-    confObj.limit = DEFAULT_LIMIT;
-    return db.book.findAll();
+    filter.limit = DEFAULT_LIMIT;
   } else {
-    confObj.limit = parseInt(limit);
+    filter.limit = parseInt(limit);
   }
+
   if (page) {
-    confObj.page = parseInt(page);
+    filter.offset = filter.limit * (parseInt(page) - 1);
+  } else {
+    filter.offset = filter.limit * (DEFAULT_PAGE - 1);
   }
+
   if (price) {
-    whereObj.price = { [Op.in]: [parseInt(price[0]), parseInt(price[1])] };
-    include.push({
-      model: db.price,
-    });
+    // add between
+    filter.where.price = {
+      [Op.between]: [parseInt(price[0]), parseInt(price[1])],
+    };
   }
+
   if (category) {
-    whereObj.categoryId = parseInt(category);
-    include.push({
+    filter.where.categoryId = parseInt(category);
+    filter.include.push({
       model: db.category,
     });
   }
 
   if (author) {
-    whereObj.authorId = parseInt(author);
-    include.push({
+    filter.where.authorId = parseInt(author);
+    filter.include.push({
       model: db.author,
     });
   }
-  console.log("whereObj>>", whereObj, "confObj>>", confObj);
-  return db.book.findAndCountAll({
-    where: whereObj,
-    offset: confObj.page,
-    limit: confObj.limit,
-  });
-};
 
-const getMyColletcion = () => {
-  const { user } = req;
-
-  return db.book.findAll({
-    where: { userId: `${user.id}` },
-  });
+  if (userId) {
+    filter.where.userId = parseInt(userId);
+  }
+  return filter;
 };
 
 module.exports = {
   bookFilter,
-  getMyColletcion,
 };
